cc98e31590686403d3494ec1c967ff1ab9f5ff0b
Annissafr <annissafitriramadhani9@gmail.com>
2025-09-20 09:52:24 +0700

    membuat login dengan breeze

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/nama', function () {
-    return "Hallo, nama saya Annissa!";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-use App\Http\Controllers\MahasiswaController;
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
-
-use App\Http\Controllers\RuanganController;
-Route::get('/ruangan', [RuanganController::class, 'index']);
-Route::post('/ruangan', [RuanganController::class, 'store']);
-
-use App\Http\Controllers\MataKuliahController;
-Route::get('/mataKuliah', [MataKuliahController::class, 'index']);
-Route::post('/mataKuliah', [MataKuliahController::class, 'store']);
+require __DIR__.'/auth.php';
